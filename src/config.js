ed711c907ea31ece227fe9e2c5f62968b267efdc
Rae Schneider <rannschneid@gmail.com>
2021-02-09 20:39:35 -0700

    updated teste and config

--- src/config.js
+++ src/config.js
@@ -3,6 +3,4 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/classify',
   TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/classify-test',
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-  "http://localhost:8000/api"
 }
