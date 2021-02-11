cf17bae376fc40f48291deae5c9b166206e5d25b
Rae Schneider <rannschneid@gmail.com>
2021-02-11 11:18:20 -0700

    updated DB to DATABASE for heruko deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/classify',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/classify-test',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/classify',
+  TEST_DATBASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/classify-test',
   API_TOKEN: process.env.API_TOKEN || 'development'
 }
