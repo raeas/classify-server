d6a95d83119b9f150a208237f02812b092e5dd64
Rae Schneider <rannschneid@gmail.com>
2021-02-11 15:15:26 -0700

    updated CLIENT_ORIGIN

--- src/app.js
+++ src/app.js
@@ -17,9 +17,9 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({
-  origin: CLIENT_ORIGIN
-})
+app.use(cors(
+    {origin: CLIENT_ORIGIN}
+  )
 )
 
 app.use(booksRouter)
