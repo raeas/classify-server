bfa4004cf6c42c717f5723e14c9248bfb972f23c
Rae Schneider <rannschneid@gmail.com>
2021-02-11 19:21:52 -0700

    updated config for CLIENT_ORIGIN for localhost

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/classify',
   TEST_DATBASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/classify-test',
-  API_TOKEN: process.env.API_TOKEN || 'development'
+  API_TOKEN: process.env.API_TOKEN || 'development',
+  CLIENT_ORIGIN: 'http://localhost:3000' 
 }
